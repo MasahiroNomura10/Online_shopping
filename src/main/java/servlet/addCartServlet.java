2512b80a7dafd60965187248bc497f45a2d821c1
相馬 崇人 <t-soma@seassist.co.jp>
2024-12-13 10:20:55 +0900

    fix:servlet

---
+++ src/main/java/servlet/addCartServlet.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.DAO.itemDAO;
+import model.entity.ItemBean;
+
+/**
+ * Servlet implementation class addCartServlet
+ */
+@WebServlet("/add-cart-servlet")
+public class addCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public addCartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("UTF-8");
+		
+		List<ItemBean> itemList = new ArrayList<>();
+		
+		itemDAO dao = new itemDAO();
+		
+		try {
+			//DAOを使って情報を取得
+			itemList = dao.cartAdd(itemId, amount);
+			System.out.println("アイテムリストを表示" + itemList);
+		} catch (SQLException | ClassNotFoundException e) {
+				e.printStackTrace();
+		}
+		
+		//リストをリクエストスコープに設定
+		request.setAttribute("itemList",itemList);
+		
+	RequestDispatcher rd = request.getRequestDispatcher("/itemList.jsp");
+	rd.forward(request, response);
+	}
+}
+
